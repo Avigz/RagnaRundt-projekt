e12f151b6794b958fe3abdaa8e87ab7b7bb57520
Talia Damary <tali0139@edu.easj.dk>
2019-05-07 11:55:58 +0200

    Nyeste gui med video embed

---
+++ Ragna-Rundt/View/Udstillingstemplate.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Editing;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Ragna_Rundt.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Udstillingstemplate : Page
+    {
+        public Udstillingstemplate()
+        {
+            this.InitializeComponent();
+            string udstillingsvideo = "<iframe width=\"1280\" height=\"720\" src=\"https://www.youtube.com/embed/s9jOm4Lbj-U\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture </iframe>";
+            VideoLink.NavigateToString(udstillingsvideo);
+        }
+    }
+}
