d35abe6fe9430a309599d3a217431a2f41ad729c
Mathias Elmhauge Petersen <math955e@edu.easj.dk>
2019-05-21 14:45:17 +0200

    Tour test opdateret

--- UnitTestProject1/UnitTest.cs
+++ UnitTestProject1/UnitTest.cs
@@ -16,8 +16,10 @@ namespace UnitTestProject1
             //I denne test vil vi vurdere hvor vidt vores autoincrement funktion ved oprettelse af et Element objekt
             // fungerer optimalt.
             //Arrange
-            Element TestElement = new Element("TestElementNavn", "TestDescription", "Http://google.dk", new Area("TestNameArea", "TestDescriptionArea", 1, 2));
-            Element TestElement1 = new Element("TestElementNavn1", "TestDescription1", "Http://google.dk1", new Area("TestNameArea1", "TestDescriptionArea1", 1, 2));
+            Element TestElement = new Element("TestElementNavn", "TestDescription", "Http://google.dk",
+                new Area("TestNameArea", "TestDescriptionArea", 1, 2));
+            Element TestElement1 = new Element("TestElementNavn1", "TestDescription1", "Http://google.dk1",
+                new Area("TestNameArea1", "TestDescriptionArea1", 1, 2));
             //Act
             int ExpectedIncrement = 1;
             int ActualIncrement = TestElement1.Id - TestElement.Id;
@@ -31,7 +33,8 @@ namespace UnitTestProject1
         {
             // I denne test undersoeger vi hvor vidt vi kan tilgaa properties paa vores Element objekt
             //Arrange
-            Element UdstillingsElement = new Element("TestElementNavn", "TestDescription", "Http://google.dk", new Area("TestNameArea", "TestDescriptionArea", 1, 2));
+            Element UdstillingsElement = new Element("TestElementNavn", "TestDescription", "Http://google.dk",
+                new Area("TestNameArea", "TestDescriptionArea", 1, 2));
 
             //Act
             string ExpectedName = "TestElementNavn";
@@ -57,7 +60,8 @@ namespace UnitTestProject1
             // I denne test vurdere vi hvor vidt vi kan tilfoeje elementer til elementCatalog
             //Arrange
             int amountOfElements = ElementCatalog.Instance.Elements.Count;
-            Element Test = new Element("TestElementNavn", "TestDescription", "Http://google.dk", new Area("TestNameArea", "TestDescriptionArea", 1, 2));
+            Element Test = new Element("TestElementNavn", "TestDescription", "Http://google.dk",
+                new Area("TestNameArea", "TestDescriptionArea", 1, 2));
 
 
             //Act
@@ -77,9 +81,10 @@ namespace UnitTestProject1
             // I denne test vurdere vi om vu korrekt kan ekstrahere oplysninger fra vores ElementCatalogSingleton
             //Arrange
             ElementCatalog TestElementCatalog = ElementCatalog.Instance;
-            Element Test = new Element("TestElementNavn", "TestDescription", "Http://google.dk", new Area("TestNameArea", "TestDescriptionArea", 1, 2));
+            Element Test = new Element("TestElementNavn", "TestDescription", "Http://google.dk",
+                new Area("TestNameArea", "TestDescriptionArea", 1, 2));
 
-            TestElementCatalog.Elements.Add(Test.Id,Test);
+            TestElementCatalog.Elements.Add(Test.Id, Test);
             //Act
             string ExpectedName = "TestElementNavn";
             string ExpectedDesc = "TestDescription";
@@ -103,8 +108,8 @@ namespace UnitTestProject1
         public void AreaPropertyTest()
         {
             //Arrange
-           Area TestArea = new Area("TestName", "TestDescription", 1,2);
-           
+            Area TestArea = new Area("TestName", "TestDescription", 1, 2);
+
             //Act
             string ExpectedName = "TestName";
             string ExpectedDesc = "TestDescription";
@@ -136,7 +141,7 @@ namespace UnitTestProject1
 
             //Assert
             Assert.AreNotEqual(CountPreAdd, CountAfterAdd);
-            Assert.AreEqual(CountPreAdd, CountAfterAdd -1);
+            Assert.AreEqual(CountPreAdd, CountAfterAdd - 1);
             Assert.AreSame(TestArea, AreaCatalog.Instance.Areas["Lydvæggen"]);
         }
 
@@ -155,8 +160,9 @@ namespace UnitTestProject1
             int ActualIdDifference = TestTag1.Id - TestTag.Id;
             //Assert
             Assert.AreEqual(ExpectedTag, TestTag.Name);
-            Assert.AreEqual(ExpectedIdDifference, ActualIdDifference );
+            Assert.AreEqual(ExpectedIdDifference, ActualIdDifference);
         }
+
         [TestMethod]
         public void TagCatalogSingletonTest()
         {
@@ -170,12 +176,12 @@ namespace UnitTestProject1
 
             //Act
             int CountPreAdd = TestTagCatalog.Tags.Count;
-            TestTagCatalog.Tags.Add("TestTagKey",TestTag);
+            TestTagCatalog.Tags.Add("TestTagKey", TestTag);
             TestTagCatalog.Tags.Add("TestTagKey1", TestTag1);
             int CountAfterAdd = TestTagCatalog.Tags.Count;
             int ExpectedCountDifference = 2;
 
-            
+
             //Assert
             Assert.AreNotEqual(CountPreAdd, CountAfterAdd);
             Assert.AreEqual(ExpectedCountDifference, CountAfterAdd - CountPreAdd);
@@ -187,16 +193,17 @@ namespace UnitTestProject1
         {
             //I dette testscenarie vil vi vurdere hvor vidt vores tour klasse kan indeholde Element objekter,
             //Samt om vi kan ekstrahere properties fra en tour, samt underelementet/ underelementerne.
-      
+
             //Arrange
-            Tour TourTestTour = new Tour("TheTestTour","Den bedste tour");
+            Tour TourTestTour = new Tour("TheTestTour", "Den bedste tour");
             Element Test = new Element("TestElementNavn", "TestDescription", "Http://google.dk", new Area("TestNameArea", "TestDescriptionArea", 1, 2));
+            
             //Act
             int CountPreAdd = TourTestTour.Elements.Count;
             TourTestTour.AddElement(Test);
             int CountAfterAdd = TourTestTour.Elements.Count;
-            int ExpectedCountDifference = 1;
-
+            int ExpectedCount = 1;
+            
             string ExpectedName = "TheTestTour";
             string ActualName = TourTestTour.Name;
 
@@ -204,8 +211,8 @@ namespace UnitTestProject1
             string ActualElementName = Test.Name;
 
             //Assert
-            Assert.AreEqual(ExpectedCountDifference, CountAfterAdd - CountPreAdd );
-            Assert.AreSame(Test, TourTestTour.Elements[CountAfterAdd]);
+            Assert.AreEqual(ExpectedCount, TourTestTour.Elements.Count);
+            Assert.AreSame(Test, TourTestTour.Elements[0]);
             Assert.AreEqual(ExpectedName, ActualName);
             Assert.AreEqual(ExpectedElementName, ActualElementName);
         }
@@ -227,6 +234,8 @@ namespace UnitTestProject1
             //Assert
 
             Assert.AreEqual(ExpectedId, ActualId);
-        
+
+        }
     }
+
 }
