ed123a18e72683d393027539b3a0238a8011faed
Mathias Elmhauge Petersen <math955e@edu.easj.dk>
2019-05-21 09:50:01 +0200

    Oprettet unittest projekt

---
+++ UnitTestProject1/UnitTest.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using Ragna_Rundt.Model;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void AddElementToElementCatalog()
+        {
+            //Arrange
+            ElementCatalog Catalog = new ElementCatalog();
+
+            //Act
+
+           //Assert
+           
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            ElementCatalog Catalog = new ElementCatalog();
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            ElementCatalog Catalog = new ElementCatalog();
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Arrange
+            ElementCatalog Catalog = new ElementCatalog();
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            //Arrange
+            ElementCatalog Catalog = new ElementCatalog();
+
+            //Act
+
+            //Assert
+        }
+
+    }
+}
