25316810bc6d728b7a0801b6c2e23a936ac93d89
qqpwn <31859931+qqpwn@users.noreply.github.com>
2019-05-22 10:29:55 +0200

    Tour + tourcatalog

--- Ragna-Rundt/Model/Tour.cs
+++ Ragna-Rundt/Model/Tour.cs
@@ -3,10 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage.Pickers.Provider;
 
-namespace Ragna_Rundt
+namespace Ragna_Rundt.Model
 {
-    class Tour
+    public class Tour
     {
+        private int _id;
+        private string _name;
+        private string _description;
+        private List<Element> _elements;
+
+        public static int autoIncrement = 1;
+
+        public Tour(string name, string description)
+        {
+            _name = name;
+            _description = description;
+            _elements = new List<Element>();
+            _id = autoIncrement;
+            autoIncrement++;
+        }
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+        }
+
+        public List<Element> Elements
+        {
+            get { return _elements; }
+        }
+
+        public void AddElement(Element element)
+        {
+            _elements.Add(element);
+        }
     }
 }
---
+++ Ragna-Rundt/Model/TourCatalog.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Ragna_Rundt.Model
+{
+    public class TourCatalog
+    {
+        private List<Tour> _tempTours;
+        private Dictionary<int, Tour> _tours;
+        private static TourCatalog _instance;
+
+        private TourCatalog()
+        {
+            _tempTours = new List<Tour>();
+
+            _tempTours.Add(new Tour("DAD Tour", "Beskrivelse"));
+            _tempTours[0].AddElement(ElementCatalog.Instance.Elements[1]);
+            _tempTours[0].AddElement(ElementCatalog.Instance.Elements[2]);
+
+            _tempTours.Add(new Tour("Kim Larsen Tour", "Beskrivelse1"));
+            _tempTours[1].AddElement(ElementCatalog.Instance.Elements[2]);
+
+
+            _tempTours.Add(new Tour("Lars Ulrichs Tour", "Beskrivelse2"));
+            _tempTours[2].AddElement(ElementCatalog.Instance.Elements[1]);
+            _tempTours[2].AddElement(ElementCatalog.Instance.Elements[2]);
+
+            _tempTours.Add(new Tour("Disco Tour", "Beskrivelse3"));
+            _tempTours[3].AddElement(ElementCatalog.Instance.Elements[3]);
+
+            _tempTours.Add(new Tour("Pladespiller Tour", "Beskrivelse4"));
+            _tempTours[4].AddElement(ElementCatalog.Instance.Elements[4]);
+
+            _tours = new Dictionary<int, Tour>();
+
+            foreach (Tour tour in _tempTours)
+            {
+                _tours.Add(tour.Id, tour);
+            }
+        }
+
+        public static TourCatalog Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new TourCatalog();
+                return _instance;
+            }
+        }
+
+        public Dictionary<int, Tour> Tours
+        {
+            get { return _tours; }
+        }
+    }
+}
